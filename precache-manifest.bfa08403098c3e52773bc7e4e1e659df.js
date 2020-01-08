963d344e663252390d14d5813f297e773d78fde9
Cas <cas@interglobal.vision>
2020-01-08 12:35:57 -0600

    Updates

--- precache-manifest.bfa08403098c3e52773bc7e4e1e659df.js
+++ precache-manifest.bfa08403098c3e52773bc7e4e1e659df.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6d180db65d2fb45244323a4f83a3e023",
+    "revision": "3b6341138c1382ac2fcc744bce4840fe",
     "url": "/index.html"
   },
   {
-    "revision": "349a63519b77fc457755",
+    "revision": "58972c44563a1b04129c",
     "url": "/static/css/main.58597dfd.chunk.css"
   },
   {
-    "revision": "9cf3d4e3806f2e4840f2",
-    "url": "/static/js/2.1971ae61.chunk.js"
+    "revision": "62c7c5b4a7585af349ca",
+    "url": "/static/js/2.495bb56f.chunk.js"
   },
   {
     "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
-    "url": "/static/js/2.1971ae61.chunk.js.LICENSE"
+    "url": "/static/js/2.495bb56f.chunk.js.LICENSE"
   },
   {
-    "revision": "349a63519b77fc457755",
-    "url": "/static/js/main.f9f86b3f.chunk.js"
+    "revision": "58972c44563a1b04129c",
+    "url": "/static/js/main.2d9724bb.chunk.js"
   },
   {
     "revision": "7a79623a2c4613688896",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.af72430adc0eadd5e85d433379aed896.js"
+  "/precache-manifest.bfa08403098c3e52773bc7e4e1e659df.js"
 );
 
 self.addEventListener('message', (event) => {
