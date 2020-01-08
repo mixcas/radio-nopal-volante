fd1f118bba399589c683dbe3507fe5db7f70cd12
Cas <cas@interglobal.vision>
2020-01-08 12:38:06 -0600

    add homepage to packages.json

---
+++ build/precache-manifest.dcb278d44e67c1e5849774b21c20b05d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c534f754a70d4895124ba90672948104",
+    "url": "/radio-nopal-volante/index.html"
+  },
+  {
+    "revision": "2513034dde246a6c55a0",
+    "url": "/radio-nopal-volante/static/css/main.58597dfd.chunk.css"
+  },
+  {
+    "revision": "62c7c5b4a7585af349ca",
+    "url": "/radio-nopal-volante/static/js/2.495bb56f.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/radio-nopal-volante/static/js/2.495bb56f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2513034dde246a6c55a0",
+    "url": "/radio-nopal-volante/static/js/main.782dba59.chunk.js"
+  },
+  {
+    "revision": "3e92c4e319a7cf5770be",
+    "url": "/radio-nopal-volante/static/js/runtime-main.1416a319.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.af72430adc0eadd5e85d433379aed896.js"
+  "/radio-nopal-volante/precache-manifest.dcb278d44e67c1e5849774b21c20b05d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/radio-nopal-volante/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
