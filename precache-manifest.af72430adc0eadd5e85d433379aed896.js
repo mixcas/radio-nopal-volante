10f8720ec818882e0a533faf7a13081ac07c7a4b
Cas <cas@interglobal.vision>
2020-01-08 12:10:57 -0600

    Updates

---
+++ precache-manifest.af72430adc0eadd5e85d433379aed896.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d180db65d2fb45244323a4f83a3e023",
+    "url": "/index.html"
+  },
+  {
+    "revision": "349a63519b77fc457755",
+    "url": "/static/css/main.58597dfd.chunk.css"
+  },
+  {
+    "revision": "9cf3d4e3806f2e4840f2",
+    "url": "/static/js/2.1971ae61.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.1971ae61.chunk.js.LICENSE"
+  },
+  {
+    "revision": "349a63519b77fc457755",
+    "url": "/static/js/main.f9f86b3f.chunk.js"
+  },
+  {
+    "revision": "7a79623a2c4613688896",
+    "url": "/static/js/runtime-main.167ee8ee.js"
+  }
+]);
